b56e7a74ca71020c4d4f2c33aa1c7941b54fa8cd
Abdelrahman Gad <Abdelrahman.Gad@student.hers.be>
2020-12-14 18:23:55 +0100

    feat(admin_page): show list of todos per user + add action to check multiple todos at once

--- djangobackend/todo/core/base/admin.py
+++ djangobackend/todo/core/base/admin.py
@@ -2,7 +2,34 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 from .models import Todo
+from django.urls import reverse
+from django.utils.http import urlencode
+from django.utils.html import format_html
 
 # Register your models here.
-admin.site.register(User, UserAdmin)
-admin.site.register(Todo)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+
+    list_display = ("myusername",)
+
+    def myusername(self, obj):
+        url = (
+            reverse("admin:todobase_todo_changelist")
+            + "?"
+            + urlencode({"owner__id": f"{obj.id}"})
+        )
+        return format_html('<a href="{}">{}</a>', url, obj.username)
+
+    myusername.short_description = "username"
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+
+    actions = ['make_checked']
+
+    def make_checked(self, request, queryset):
+        queryset.update(checked=True)
+
+    make_checked.short_description = "Check selected todos"
